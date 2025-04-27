41d0d4270415650875f7e41227a0d77ef9f95ac2
havenwhitney <havenw@stanford.edu>
2025-04-27 14:56:08 -0700

    Working on toolbar and changed aspect ratio of viewport

---
+++ scripts/toolbar.gd
@@ -0,0 +1,18 @@
+extends Control
+
+func _on_rectangle_pressed() -> void:
+    pass # Replace with function body.
+    
+func on_circle_pressed() -> void:
+    print("Not Implemented")
+
+func on_triangle_pressed() -> void:
+    print("Not Implemented")
+
+func on_trapezoid_pressed() -> void:
+    print("Not Implemented")
+
+func on_objective_pressed() -> void:
+    print("Not Implemented")
+
+
