a431f39991070965dc357d541532e1185c0b1598
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2025-04-27 16:40:34 -0700

    play/pause button

---
+++ scripts/play_pause_button.gd
@@ -0,0 +1,23 @@
+extends TextureButton
+
+var is_playing: bool = false
+
+@export var texture_play: CompressedTexture2D
+@export var texture_pause: CompressedTexture2D
+
+signal play
+signal pause
+
+func _on_pressed() -> void:
+	if is_playing:
+		emit_signal("pause")
+	else:
+		emit_signal("play")
+
+func _on_play() -> void:
+	is_playing = true
+	texture_normal = texture_play
+
+func _on_pause() -> void:
+	is_playing = false
+	texture_normal = texture_pause
