ca2bd542bcd86c9c32e8bb9562ef42f5d4487ea6
havenwhitney <havenw@stanford.edu>
2025-04-27 17:59:55 -0700

    Updated to use draggable script instead of directly dragging on obstacle

--- scripts/draggable.gd
+++ scripts/draggable.gd
@@ -1,40 +1,33 @@
 extends Node2D
 
-# var draggable = false
+var draggable = false
 
 # make static cur object
-# static var cur_object = null
+static var cur_object = null
 
 
-# func _process(delta: float) -> void:
-# 	if draggable:
-# 		# Check if the mouse is pressed
-# 		if Input.is_action_pressed("click"):
-# 			# Check if the mouse is over the obstacle
-# 			if not cur_object or cur_object == self:
-# 				# Cur object is the object that was clicked
-# 				cur_object = self
-# 				var mouse_pos = get_global_mouse_position()
-# 				global_position = mouse_pos
+func _process(delta: float) -> void:
+	if draggable:
+		# Check if the mouse is pressed
+		if Input.is_action_pressed("click"):
+			# Check if the mouse is over the obstacle
+			if not cur_object or cur_object == get_parent():
+				# Cur object is the object that was clicked
+				cur_object = get_parent()
+				var mouse_pos = get_global_mouse_position()
+				cur_object.global_position = mouse_pos
 
-# 	# Check if the mouse is released
-# 	if Input.is_action_just_released("click"):
-# 		cur_object = null
-# 		print("RELEASE")
+	# Check if the mouse is released
+	if Input.is_action_just_released("click"):
+		cur_object = null
+		print("RELEASE")
 		
-# 		print("HOVERING OVER OBSTACLE")
+		print("HOVERING OVER OBJECT")
 
-# func get_bonked():
-# 	$AudioStreamPlayer2D.play()
-# 	print("Bonk!")
-	
-# 	# TODO: Play sound
+func _on_mouse_obstacle_entered(shape_idx: int) -> void:
+	draggable = true
+	print("Mouse entered object")
 
-
-# func _on_mouse_obstacle_entered(shape_idx: int) -> void:
-# 	draggable = true
-# 	print("Mouse entered obstacle")
-
-# func _on_mouse_obstacle_exited(shape_idx: int) -> void:
-# 	draggable = false
-# 	print("Mouse exited obstacle")
+func _on_mouse_obstacle_exited(shape_idx: int) -> void:
+	draggable = false
+	print("Mouse exited object")
--- scripts/obstacle.gd
+++ scripts/obstacle.gd
@@ -1,29 +1,29 @@
 class_name Obstacle extends StaticBody2D
 
-var draggable = false
+# var draggable = false
 
-# make static cur object
-static var cur_object = null
+# # make static cur object
+# static var cur_object = null
 
-# put $PlayPauseButton in scene tree, directly ask its global variable for if something is paused
+# # put $PlayPauseButton in scene tree, directly ask its global variable for if something is paused
 
-func _process(delta: float) -> void:
-	if draggable:
-		# Check if the mouse is pressed
-		if Input.is_action_pressed("click"):
-			# Check if the mouse is over the obstacle
-			if not cur_object or cur_object == self:
-				# Cur object is the object that was clicked
-				cur_object = self
-				var mouse_pos = get_global_mouse_position()
-				global_position = mouse_pos
+# func _process(delta: float) -> void:
+# 	if draggable:
+# 		# Check if the mouse is pressed
+# 		if Input.is_action_pressed("click"):
+# 			# Check if the mouse is over the obstacle
+# 			if not cur_object or cur_object == self:
+# 				# Cur object is the object that was clicked
+# 				cur_object = self
+# 				var mouse_pos = get_global_mouse_position()
+# 				global_position = mouse_pos
 
-	# Check if the mouse is released
-	if Input.is_action_just_released("click"):
-		cur_object = null
-		print("RELEASE")
+# 	# Check if the mouse is released
+# 	if Input.is_action_just_released("click"):
+# 		cur_object = null
+# 		print("RELEASE")
 		
-		print("HOVERING OVER OBSTACLE")
+# 		print("HOVERING OVER OBSTACLE")
 
 func get_bonked():
 	$AudioStreamPlayer2D.play()
@@ -32,10 +32,10 @@ func get_bonked():
 	# TODO: Play sound
 
 
-func _on_mouse_obstacle_entered(shape_idx: int) -> void:
-	draggable = true
-	print("Mouse entered obstacle")
+# func _on_mouse_obstacle_entered(shape_idx: int) -> void:
+# 	draggable = true
+# 	print("Mouse entered obstacle")
 
-func _on_mouse_obstacle_exited(shape_idx: int) -> void:
-	draggable = false
-	print("Mouse exited obstacle")
+# func _on_mouse_obstacle_exited(shape_idx: int) -> void:
+# 	draggable = false
+# 	print("Mouse exited obstacle")
