56f566f95055611ebe8188edd87127141623da75
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2025-05-13 15:33:06 -0700

    fix bug with win modal not disappearing and other inconsistent behavior

--- scripts/counter_label.gd
+++ scripts/counter_label.gd
@@ -6,23 +6,22 @@ extends RichTextLabel
 var total_parts := 0
 
 func _ready():
-	print("readylabel")
-	if tracker:
-		print("foundtracker")
-		total_parts = get_tree().get_nodes_in_group("collectibles").size()
-		var collectibles = get_tree().get_nodes_in_group("collectibles")
-		for collectible in collectibles:
-			print("Connecting collectible:", collectible.name)
-			collectible.connect("collected", $CollectedTracker.increment)
-		
-		print("label Found collectibles:", collectibles.size())
-		tracker.connect("parts_updated", _on_parts_updated)
-		
-		text = "Collected: 0 / %d" % total_parts
+	if not tracker:
+		return
+
+	var collectibles = get_tree().get_nodes_in_group("collectibles")
+	for collectible in collectibles:
+		collectible.connect("collected", $CollectedTracker.increment)
+
+	tracker.connect("parts_updated", _on_parts_updated)
+
+	total_parts = collectibles.size()
+	text = "Collected: 0 / %d" % total_parts
 
 func _on_parts_updated(collected: int):
+	if (total_parts == 0):
+		return
 	text = "Collected: %d / %d" % [collected, total_parts]
 	if collected == total_parts:
 		#probably edit to add a different event from win
 		EventBus.emit_signal("win")
-	
