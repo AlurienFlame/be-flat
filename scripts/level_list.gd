f955115426f2395cc8f639637bb4a4dde17230ee
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2025-05-13 15:02:15 -0700

    disable all but first level buttons in puzzles screen

--- scripts/level_list.gd
+++ scripts/level_list.gd
@@ -3,9 +3,13 @@ extends HFlowContainer
 @onready var puzzle_button: PackedScene = preload("res://scenes/UI/puzzle_button.tscn")
 
 func _ready() -> void:
-	var levels = DirAccess.get_files_at("res://scenes/levels")
-	for filename in levels:
-		var button: AspectRatioContainer = puzzle_button.instantiate()
-		button.get_node("Button").text = filename
-		button.index = filename.to_int()
-		add_child(button)
+    var levels = DirAccess.get_files_at("res://scenes/levels")
+    var unlocked_first_level = false
+    for filename in levels:
+        var button: AspectRatioContainer = puzzle_button.instantiate()
+        add_child(button)
+        if unlocked_first_level:
+            button.init(filename, false)
+        else:
+            button.init(filename, true)
+            unlocked_first_level = true
