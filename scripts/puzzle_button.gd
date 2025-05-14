c646e364faa95d26a527dcb6443547dacc629f22
simrantan <simrant@stanford.edu>
2025-05-13 17:31:06 -0700

    fixed triangle!

--- scripts/puzzle_button.gd
+++ scripts/puzzle_button.gd
@@ -6,25 +6,25 @@ class_name PuzzleButton extends AspectRatioContainer
 @onready var scene_manager = $"/root/SceneManager"
 
 func _ready() -> void:
-    # Thumbnails don't work because the instantiated scene UI breaks the menu UI
-    # And we can't create the scene without UI because too much depends on it
-    # var instantiated = scene.instantiate()
-    # subviewport.add_child(instantiated)
-    # await RenderingServer.frame_post_draw
-    # button.icon = subviewport.get_texture()
-    # instantiated.queue_free()
-    pass
+	# Thumbnails don't work because the instantiated scene UI breaks the menu UI
+	# And we can't create the scene without UI because too much depends on it
+	# var instantiated = scene.instantiate()
+	# subviewport.add_child(instantiated)
+	# await RenderingServer.frame_post_draw
+	# button.icon = subviewport.get_texture()
+	# instantiated.queue_free()
+	pass
 
 func init(filename, enabled) -> void:
-    button.text = filename
-    index = filename.to_int()
-    button.disabled = !enabled
+	button.text = filename
+	index = filename.to_int()
+	button.disabled = !enabled
 
 func _on_button_pressed() -> void:
-    scene_manager.load_level(index)
+	scene_manager.load_level(index)
 
 func unlock() -> void:
-    button.disabled = false
+	button.disabled = false
 
 func is_unlocked() -> bool:
-    return !button.disabled
+	return !button.disabled
