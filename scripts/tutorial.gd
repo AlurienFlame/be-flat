1db333df849a4e4cfe50a0189130432f5a41bd08
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2025-05-15 11:55:20 -0700

    add level 2 tutorial text

--- scripts/tutorial.gd
+++ scripts/tutorial.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
 func _ready() -> void:
+    Dialogic.process_mode = Node.PROCESS_MODE_ALWAYS
+    EventBus.connect("lose", _on_lose)
+
     if Dialogic.current_timeline != null:
         return
     Dialogic.start("tutorial").process_mode = Node.PROCESS_MODE_ALWAYS
-    Dialogic.process_mode = Node.PROCESS_MODE_ALWAYS
-    
-    EventBus.connect("lose", _on_lose)
 
 func _on_lose():
+    if Dialogic.current_timeline != null:
+        return
     Dialogic.start("tutorial-you-lost").process_mode = Node.PROCESS_MODE_ALWAYS
