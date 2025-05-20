5a2be06463980d04985acd31b81c99191b9e01de
simrantan <simrant@stanford.edu>
2025-05-19 17:38:42 -0700

    audio fixed!

--- scripts/puzzle_button.gd
+++ scripts/puzzle_button.gd
@@ -4,7 +4,7 @@ class_name PuzzleButton extends AspectRatioContainer
 
 @export var index: int
 @onready var scene_manager = $"/root/SceneManager"
-@onready var menuaudio = $"root/SceneManager/MainMenu/AudioStreamPlayer"
+@onready var menuaudio = get_node("/root/SceneManager/MainMenu/AudioStreamPlayer")
 
 func _ready() -> void:
     # Thumbnails don't work because the instantiated scene UI breaks the menu UI
@@ -22,7 +22,7 @@ func init(filename, enabled) -> void:
     button.disabled = !enabled
 
 func _on_button_pressed() -> void:
-
+    menuaudio.stop()
     scene_manager.load_level(index)
 
 func unlock() -> void:
